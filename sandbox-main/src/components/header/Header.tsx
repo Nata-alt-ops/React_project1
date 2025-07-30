67cea84159e73983b913e39e3152ea127d8e8f1c
n <nataliaklucnikova9@gmail.com>
2025-07-30 10:27:12 +0300

    Добавлен логотип и кнопки(не прописанна логика)

--- sandbox-main/src/components/header/Header.tsx
+++ sandbox-main/src/components/header/Header.tsx
@@ -16,15 +16,27 @@ export const Header = () => {
       name: 'Новости',
       path: '/news',
     },
+    {
+      name: 'О нас',
+      path: '/about_us',
+    },
     ];
 
   return (
-    <div className={'header'}>
-      {navigateMenu.map((menu) => (
-        <div onClick={() => navigate(menu.path)} className={isActive(menu.path) ? 'navigate-active' : 'navigate'}>
-          <>{menu.name}</>
+    <div className={'header_con'}>
+      <div className={'logo_nav_con'}>
+        <div className={'logo'}>
+          <div className='cirius'></div>
+          <p className='logo_text'>Logo</p>
         </div>
-      ))}
+        <div className={'header'}>
+          {navigateMenu.map((menu) => (
+            <div onClick={() => navigate(menu.path)} className={isActive(menu.path) ? 'navigate-active' : 'navigate'}>
+              <>{menu.name}</>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 };
