82077fe6b7916ae865cbd168dfa8430b676fc4ad
n <nataliaklucnikova9@gmail.com>
2025-08-05 15:19:58 +0300

    .

---
+++ sandbox-main/src/pages/login/Login.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+
+/*Типы данных для полей ввода*/ 
+type LoginPassword = {
+    username: string;
+    password: string;
+};
+
+export const Login = () =>{
+    /*Для перехода между страницами*/
+    const nav = useNavigate();
+    const { 
+    register, handleSubmit, formState:{ errors }, setError } = useForm<LoginPassword>();
+
+    /*Фун*/ 
+
+} 
+
